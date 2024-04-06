a61e2178ee01006ac3f767d70c8d8ba6ed3d35ae
carissaor <carissaor@gmail.com>
2024-04-06 16:03:50 -0700

    fix nav

--- frontend/App.js
+++ frontend/App.js
@@ -1,43 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Hikes from './components/hikes'; // Adjust the path accordingly
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Hikes /> {/* Render the Hikes component */}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+// import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import SignUpPage from './scenes/SignUpPage';
-// import HikesPage from './scenes/HikesPage';
-
-// const Stack = createStackNavigator();
+// import Hikes from './components/hikes'; // Adjust the path accordingly
 
 // export default function App() {
 //   return (
-//   <NavigationContainer>
-//     <Stack.Navigator initialRouteName="SignIn">
-//       {/* should be the sign in page just doing this because I want to set up navigation */}
-//       <Stack.Screen name="SignIn" component={SignUpPage} /> 
-//       <Stack.Screen name="Hikes" component={HikesPage} />
-//     </Stack.Navigator>
-//   </NavigationContainer>
-// );
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <Hikes /> {/* Render the Hikes component */}
+//     </View>
+//   );
 // }
 
 // const styles = StyleSheet.create({
@@ -48,3 +19,32 @@ const styles = StyleSheet.create({
 //     justifyContent: 'center',
 //   },
 // });
+
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignUpPage from './scenes/SignUpPage';
+import HikesPage from './scenes/HikesPage';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="SignIn">
+      {/* should be the sign in page just doing this because I want to set up navigation */}
+      <Stack.Screen name="SignIn" component={SignUpPage} /> 
+      <Stack.Screen name="Hikes" component={HikesPage} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- frontend/scenes/HikesPage.js
+++ frontend/scenes/HikesPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Hikes from '../components/hikes';
 
 const HikesPage = () => {
   return (
     <View style={styles.container}>
-      <Text>Hikes Page</Text>
+      <Hikes /> 
     </View>
   );
 };
